bbca25a6535e1805480f3e9c918de8e6cf86895a
pshen <pshenyshniuk@gmail.com>
2024-04-24 20:47:56 +0200

    Added .gitignore file

--- kostya test/test.py
+++ kostya test/test.py
@@ -1,3 +1,7 @@
-minimum_width = int(input('minimum_width :'))
-maximum_width = int(input('maximum_width :'))
-print((maximum_width - minimum_width) / 2)
+# minimum_width = int(input('minimum_width :'))
+# maximum_width = int(input('maximum_width :'))
+# print((maximum_width - minimum_width) / 2)
+
+param1 = list(range(3, 9, 2))
+print(param1[0])
+print(param1)
