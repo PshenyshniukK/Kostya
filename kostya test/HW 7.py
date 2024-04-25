08a203df4b6049447ecf000f28cb6acc9547789d
pshen <pshenyshniuk@gmail.com>
2024-04-25 09:22:14 +0200

    Added .gitignore file

--- kostya test/HW 7.py
+++ kostya test/HW 7.py
@@ -1,4 +1,4 @@
 Number_lines = int(input('Number lines is :'))
-for i in range(Number_lines+1):
+for i in range(Number_lines):
     if i == i:
-        print(' ' * (Number_lines - i) + '*' * (i+1))
+        print(' ' * (Number_lines - i - 1) + '*' * (i+1))
