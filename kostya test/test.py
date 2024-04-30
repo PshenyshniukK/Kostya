62c864c9df078f81fbb34f7327e5428ef2c79f32
pshen <pshenyshniuk@gmail.com>
2024-04-30 21:47:23 +0200

    Added .gitignore file

--- kostya test/test.py
+++ kostya test/test.py
@@ -1,16 +1,19 @@
+#
+# string1 = input()
+# str2 = int(len(string1))
+# result = ''
+# for i in range(str2 -1, -1, -1):
+#     if i == i:
+#         result += string1[i]
+#
+# string1 = string1.replace(' ', '')
+# string1 = string1.lower()
+# result = result.replace(' ', '')
+# result = result.lower()
+# print(string1)
+# print(result)
+#
+# print(string1 == result)
 
-string1 = input()
-str2 = int(len(string1))
-result = ''
-for i in range(str2 -1, -1, -1):
-    if i == i:
-        result += string1[i]
-
-string1 = string1.replace(' ', '')
-string1 = string1.lower()
-result = result.replace(' ', '')
-result = result.lower()
-print(string1)
-print(result)
-
-print(string1 == result)
+str = "Qwerty"
+print(str.find('Q'))
