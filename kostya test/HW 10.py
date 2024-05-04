0642635de7fcbb32002bc814d284ee9fa75df400
pshen <pshenyshniuk@gmail.com>
2024-05-05 00:03:09 +0200

    Added .gitignore file

--- kostya test/HW 10.py
+++ kostya test/HW 10.py
@@ -5,7 +5,7 @@ result = ''
 for i in range(str2 - 1, -1, -1):
     result += string1[i]
 result2 = result
-print(string1)
-print(result)
-print(result2)
+# print(string1)
+# print(result)
+# print(result2)
 print(string1 == result2)
