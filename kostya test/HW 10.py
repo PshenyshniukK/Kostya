e834d030adc2c5ba3b66b160f494151a10b40c10
pshen <pshenyshniuk@gmail.com>
2024-05-04 23:15:18 +0200

    Added .gitignore file

--- kostya test/HW 10.py
+++ kostya test/HW 10.py
@@ -1,12 +1,13 @@
 
-string1 = input().lower().replace(' ', '')
+string1 = input().lower().lstrip().rstrip()
 str2 = int(len(string1))
 result = ''
 for i in range(str2 -1, -1, -1):
     if i == i:
         result += string1[i]
-
+result2 = result
 print(string1)
 print(result)
+print(result2)
 
-print(string1 == result)
+print(string1 == result2)
--- kostya test/test.py
+++ kostya test/test.py
@@ -1,6 +1,26 @@
+#
+# print("ab12".isalnum())
+# print("ab_12".isalnum())
+# print("ab".isalpha())
+# print("ab12".isalpha())
+# print("ab12\n\tQ".isascii())
 
-print("ab12".isalnum())
-print("ab_12".isalnum())
-print("ab".isalpha())
-print("ab12".isalpha())
-print("ab12\n\tQ".isascii())
+# print("Modification")
+# par1 = ' DEBUG saved to db   '
+# print(par1.removeprefix(" "))
+#
+# print("DEBUG saved to db".removeprefix("INFO"))
+# print("abcdqq".removesuffix('qq'))
+# print("abcdqq 1".removesuffix(' '))
+# str1 = "ababbbabaaab"
+# str2 = str1.replace('ab', 'xx')
+# print(str1)
+# print(str2)
+# print("ababbbabaaab".replace('ab', 'xx'))
+# print("ababbbabaaab".replace('ab', 'xx', 2))
+# print('01\t012\t0123\t01234'.expandtabs())
+# print('01\t012\t0123\t01234'.expandtabs(4))
+
+str1 = ' aba '
+str2 = str.removesuffix(str1,' ')
+print(str2)
