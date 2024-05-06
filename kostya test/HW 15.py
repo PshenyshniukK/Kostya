e735fed71c8988e04d48e5f4141a4285b762494c
pshen <pshenyshniuk@gmail.com>
2024-05-06 22:05:27 +0200

    Added .gitignore file

--- kostya test/HW 15.py
+++ kostya test/HW 15.py
@@ -1,4 +1,12 @@
 lst = [2, 4, 6, 2, 1, 1, 9, 4, 6]
-print('min = ', min(lst))
-print('max = ', max(lst))
-print('sum_ = ', sum(lst), 'product = ')
+min = 3
+max = 6
+for i in lst.copy():
+    if i <= min or i >= max:
+        lst.remove(i)
+print(sum(lst))
+
+result = 1
+for x in lst:
+    result = result * x
+print(result)
